2298ac4321e03caf24e5c42f13d5da7b60e587e5
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2024-11-07 22:15:53 +1300

    moar

--- micromanagermchghee/src/main.rs
+++ micromanagermchghee/src/main.rs
@@ -4,15 +4,46 @@ use windows::{
     Win32::System::Com::*,
     Win32::UI::Shell::*,
     Win32::Foundation::HWND,
+    Win32::Foundation::HANDLE,
     Win32::UI::WindowsAndMessaging::GetForegroundWindow
 };
 use std::{
     io::{self, Write},
     thread::sleep,
     time::Duration,
+    env,
+    fs
 };
 use windows::Win32::Foundation::GetLastError;
 
+/* UI */
+slint::slint!{
+    export component Timer inherits Window {
+        in property <string> display_text;
+
+        preferred-width: 300px;
+        preferred-height: 100px;
+        title: "Micromanager Mcghee";
+        no-frame: true;
+        background: transparent;
+        always-on-top: true;
+
+        Text {
+            text: root.display_text;
+            color: white;
+            font-family: "Calibri";
+            font-size: 24px;
+        }
+    }
+}
+
+/* File System */
+fn check_program_folder() -> Result<()> {
+    let app_folder = format!("{}\\Micromanager Mcghee", env::var("LOCALAPPDATA").unwrap_or_default());
+    Ok(fs::create_dir_all(app_folder)?)
+}
+
+/* Virtual Desktops */
 struct VirtualDesktopManager {
     manager: IVirtualDesktopManager,
 }
@@ -30,26 +61,34 @@ impl VirtualDesktopManager {
         Ok(Self { manager })
     }
 
-    // fn is_window_on_current_desktop(&self, hwnd: HWND) -> Result<bool> {
-    //     unsafe { Ok(self.manager.IsWindowOnCurrentVirtualDesktop(hwnd)?.as_bool()) }
-    // }
+    fn is_window_on_current_desktop(&self, hwnd: HWND) -> Result<bool> {
+        unsafe { Ok(self.manager.IsWindowOnCurrentVirtualDesktop(hwnd)?.as_bool()) }
+    }
 
     fn get_window_desktop_id(&self, hwnd: HWND) -> Result<GUID> {
         unsafe { self.manager.GetWindowDesktopId(hwnd) }
     }
 }
 fn getdesktopid() -> Option<GUID> {
-
-    // Retrieve
     let result = (|| {
         let vdm = VirtualDesktopManager::new().ok()?;
         let hwnd = unsafe { GetForegroundWindow() };
+
+        // Make sure the window is on the new desktop
+        vdm.is_window_on_current_desktop(hwnd).ok();
         vdm.get_window_desktop_id(hwnd).ok()
     })();
 
     result
 }
+
 fn main() {
+
+    // Init Storage
+    if check_program_folder().is_err() {
+        println!("Error creating folder");
+        return;
+    };
 
     // Init Memory
     let mut desktops = std::collections::HashMap::with_capacity(8);
@@ -57,25 +96,41 @@ fn main() {
         println!("CoInitializeEx failed with error: {:?}", unsafe { GetLastError() });
     };
 
+    // Init Timer
+    let testing = "Independent Assessor";
+    let timer = Timer::new().unwrap();
+    timer.set_display_text(testing.into());
+
+    let window = timer.window();
+    window.set_position(slint::PhysicalPosition::new(1600,0)); // Window Positiom
+
+    let weak = timer.as_weak();
+
+    timer.run().unwrap(); // Start
+
     // Core
     loop {
 
-        // Retrieve ID
         let Some(current_desktop_id) = getdesktopid() else {
-            eprintln!("Failed to get desktop ID");
-            return;
+            continue;
         };
 
-        // Desktop Change
         if !desktops.contains_key(&current_desktop_id) {
+
+            weak.upgrade_in_event_loop(move |handle| {
+                handle.set_display_text("New Desktop".into());
+            }).unwrap();
+
             print!("\nNew Desktop ID. Enter an alias: ");
             io::stdout().flush().unwrap();
 
-            let mut input = String::with_capacity(32);
+            let mut input = String::with_capacity(32); // Get New Alias
             if io::stdin().read_line(&mut input).is_ok() {
                 desktops.insert(current_desktop_id, input.trim().to_string());
             }
-            println!("Created New Desktop: {}", input);
+            weak.upgrade_in_event_loop(move |handle| { // Update Text
+                handle.set_display_text(input.into());
+            }).unwrap();
             println!("Current Desktops:");
              for (key, value) in desktops.iter() {
                 println!("{key:?}: {value}");
@@ -86,5 +141,4 @@ fn main() {
     }
 
     unsafe { CoUninitialize() };
-
 }
