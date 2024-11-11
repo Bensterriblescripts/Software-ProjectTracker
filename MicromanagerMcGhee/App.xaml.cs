6186d287aa4174d3d3e821d6d01f55c3e42e57bf
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2024-11-12 11:30:56 +1300

    C# version, screw the borrow checker

---
+++ MicromanagerMcGhee/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+namespace MicromanagerMcGhee {
+    public partial class App : Application {
+
+        protected override void OnStartup(StartupEventArgs e) {
+
+            // Software Rendering
+            base.OnStartup(e);
+            RenderOptions.ProcessRenderMode = RenderMode.SoftwareOnly;
+
+            // Virtual Desktop
+
+        }
+    }
+}
